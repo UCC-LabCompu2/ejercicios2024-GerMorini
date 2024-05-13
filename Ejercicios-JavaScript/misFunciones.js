9fc172550d538c2a2d83f755a7ebc31f969f550b
GerMorini <2302130@ucc.edu.ar>
2024-05-13 10:30:30 -0300

    Hecho ejercicios de javascript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,30 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function convertirUnidades(nombre, valor) {
+    if (isNaN(valor)) {
+        alert("El valor ingresado no es un número!")
+        document.getElementById('metro').value = 0
+        document.getElementById('pulgada').value = 0
+        document.getElementById('pie').value = 0
+        document.getElementById('yarda').value = 0
+    } else {
+        if (nombre === "metro") {
+            document.getElementById('pulgada').value = valor * 39.3701
+            document.getElementById('pie').value = valor * 3.28084
+            document.getElementById('yarda').value = valor * 1.093
+        } else if (nombre === "pulgada") {
+            document.getElementById('metro').value = valor * 39.3701
+            document.getElementById('pie').value = valor * 3.28084
+            document.getElementById('yarda').value = valor * 1.093
+        } else if (nombre === "pie") {
+            document.getElementById('pulgada').value = valor * 39.3701
+            document.getElementById('metro').value = valor * 3.28084
+            document.getElementById('yarda').value = valor * 1.093
+        } else if (nombre === "yarda") {
+            document.getElementById('pulgada').value = valor * 39.3701
+            document.getElementById('pie').value = valor * 3.28084
+            document.getElementById('metro').value = valor * 1.093
+        }
+    }
+}
