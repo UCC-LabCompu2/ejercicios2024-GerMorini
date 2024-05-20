aef082551afb718656bc063f2bc7d051829f4bca
GerMorini <2302130@ucc.edu.ar>
2024-05-20 08:44:16 -0300

    Hecho ejercicio de refactorización

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -4,30 +4,55 @@
  * @param {string} nombre - ID del elemento en el HTML
  * @param {number} valor - valor ingresado por el usuario
  */
-function convertirUnidades(nombre, valor) {
+
+// TODO: asignar las conversiónes correctas para las demás unidades
+convertirUnidades = (nombre, valor) => {
+    let valMetro, valPulgada, valPie, valYarda
+
     if (isNaN(valor)) {
         alert("El valor ingresado no es un número!")
-        document.getElementById('metro').value = 0
-        document.getElementById('pulgada').value = 0
-        document.getElementById('pie').value = 0
-        document.getElementById('yarda').value = 0
+        valMetro = ""
+        valPulgada = ""
+        valPie = ""
+        valYarda = ""
     } else {
         if (nombre === "metro") {
-            document.getElementById('pulgada').value = valor * 39.3701
-            document.getElementById('pie').value = valor * 3.28084
-            document.getElementById('yarda').value = valor * 1.093
+            valMetro = valor
+            valPulgada = valor * 39.3701
+            valPie = valor * 3.28084
+            valYarda = valor * 1.093
         } else if (nombre === "pulgada") {
-            document.getElementById('metro').value = valor * 39.3701
-            document.getElementById('pie').value = valor * 3.28084
-            document.getElementById('yarda').value = valor * 1.093
+            valPulgada = valor
+            valMetro = valor * 39.3701
+            valPie = valor * 3.28084
+            valYarda = valor * 1.093
         } else if (nombre === "pie") {
-            document.getElementById('pulgada').value = valor * 39.3701
-            document.getElementById('metro').value = valor * 3.28084
-            document.getElementById('yarda').value = valor * 1.093
+            valPie = valor
+            valPulgada = valor * 39.3701
+            valMetro = valor * 3.28084
+            valYarda = valor * 1.093
         } else if (nombre === "yarda") {
-            document.getElementById('pulgada').value = valor * 39.3701
-            document.getElementById('pie').value = valor * 3.28084
-            document.getElementById('metro').value = valor * 1.093
+            valYarda = valor
+            valPulgada = valor * 39.3701
+            valPie = valor * 3.28084
+            valMetro = valor * 1.093
         }
+    }
+
+    document.getElementById('metro').value = valMetro
+    document.getElementById('pulgada').value = valPulgada
+    document.getElementById('pie').value = valPie
+    document.getElementById('yarda').value = valYarda
+}
+
+function convertirGR(id) {
+    if (id === "grados") {
+        let grad = document.getElementById("grados").value
+        let rad = grad*Math.PI/180
+        document.getElementById("radianes").value = rad
+    } else if (id === "radianes") {
+        let rad = document.getElementById("radianes").value
+        let grad = rad*180/Math.PI
+        document.getElementById("grados").value = grad
     }
 }
